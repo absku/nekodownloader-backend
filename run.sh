aeabaf0295c63a8341097e2486b256f708052cce
Marcos Pedaci <marcospedaci@gmail.com>
2024-04-07 01:24:25 -0300

    add deploy command

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ sudo docker build -t nekodownloader:latest .
 
 # Test image
 # sudo docker run --rm -it -v mangas:/mnt/mangas nekodownloader:latest ls -l /mnt/media
+
+# Deploy
+sudo docker run -v mangas:/mnt/mangas -p 1200:8080 nekodownloader-front:latest
